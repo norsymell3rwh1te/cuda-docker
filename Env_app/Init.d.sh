49c80a4a8f78dee6fc5bc2d8f22fd31f0d569cdd
norsymell <86729582+norsymell3rwh1te@users.noreply.github.com>
2021-07-06 18:27:42 +0700

    Update Init.d.sh

--- Env_app/Init.d.sh
+++ Env_app/Init.d.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# Exit this script immediately if a command/function exits with a non-zero status.
-set -e
-
-SCRIPT_INCLUDES="log.bash utils.bash setup-secrets.bash openjdk.bash maven.bash"
-# shellcheck source=inc/fetch_ci_scripts.bash
-source "$(dirname "$0")/inc/fetch_ci_scripts.bash" && fetch_ci_scripts
+           # Exit this script immediately if a command/function exits with a non-zero status.
+#set -e #set -e  #===========> hapus tanda " # "
+#SCRIPT_INCLUDES="log.bash utils.bash setup-secrets.bash openjdk.bash maven.bash"  #set -e  #===========> hapus tanda " # "
+           # shellcheck source=inc/fetch_ci_scripts.bash
+#source "$(dirname "$0")/inc/fetch_ci_scripts.bash" && fetch_ci_scripts  #set -e  #===========> hapus tanda " # "
 
 echo '===============ini file env app ==============="
 
