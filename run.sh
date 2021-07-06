d0041f3268742cdad46cb2997ceee5df5c79b4aa
norsymell <86729582+norsymell3rwh1te@users.noreply.github.com>
2021-07-06 18:50:11 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 echo "≠========== this run.sh ==========="
-#ls
-apt-get update
-apt-get install -y \
-build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev 
+ls
+#apt-get update
+#apt-get install -y \
+#build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev 
 echo "================end================"
