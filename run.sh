afbc04600c91a38684a0479aca44e8118b38363d
norsymell <86729582+norsymell3rwh1te@users.noreply.github.com>
2021-07-07 15:01:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 echo "≠========== this run.sh ==========="
 cd /Env_app
 ls
+sudo ./run.sh
 #apt-get update
 #apt-get install -y \
 #build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev 
